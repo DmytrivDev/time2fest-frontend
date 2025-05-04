0ec11cc1f713b40b2062300ae4b19eaa2f0fb75f
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-05-04 18:52:49 +0300

    test 2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-const HomePage = () => 
+const HomePage = () => {
   return (
     <div className="p-4">
       <h1>Hello World 🎉</h1>
       <p>Welcome to Time2Fest Frontend !</p>
     </div>
-  );
+  
 };
 
 export default HomePage;
