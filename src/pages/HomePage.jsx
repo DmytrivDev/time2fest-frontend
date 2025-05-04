9490d8778da95ec08bd23396577ec99846c5eac5
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-05-04 18:39:09 +0300

    ✅ husky finally alive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div className="p-4">
       <h1>Hello World 🎉</h1>
-      <p>Welcome to Time2Fest Frontend!</p>
+      <p>Welcome to Time2Fest Frontend    !</p>
     </div>
   );
 };
