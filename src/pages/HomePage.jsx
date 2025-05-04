f0e45e4b155ec1b1e79ec3e043370f857a90144e
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-05-04 17:08:29 +0300

    Ініціалізація проєкту на Vite

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div className="p-4">
+        <h1>Hello World 🎉</h1>
+        <p>Welcome to Time2Fest Frontend</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
