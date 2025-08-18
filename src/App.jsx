c9c412910cf21a38ccb6193bd6f2981056120b4e
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-08-18 06:42:05 +0300

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
+        <Route 
           path="/"
           element={
             isSupported && detectedLang !== DEFAULT_LANG ? (
