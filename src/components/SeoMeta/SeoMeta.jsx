6bd0cc7edb4a2f60f0d5effd883cd08798be73be
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-09-07 14:58:02 +0300

    Favicon

--- src/components/SeoMeta/SeoMeta.jsx
+++ src/components/SeoMeta/SeoMeta.jsx
@@ -64,6 +64,21 @@ const SeoMeta = () => {
       ))}
 
       <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      <link
         rel="preload"
         href="/fonts/Roboto/Roboto-Regular.woff2"
         as="font"
